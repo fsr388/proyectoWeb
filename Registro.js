69c0929f5b96b7b6305ae096ab32e69f4a4878aa
C06505 <FSROCA@hipotecario.com.ar>
2023-07-17 21:36:40 -0300

    Sengunda entrega js

---
+++ Registro.js
@@ -0,0 +1,32 @@
+var nombre = "";
+var apellido = "";
+var email = "";
+var contraseña = "";
+
+function solicitarDatos() {
+  nombre = prompt("Ingrese su nombre:");
+  apellido = prompt("Ingrese su apellido:");
+  email = prompt("Ingrese su correo electrónico:");
+  contraseña = prompt("Ingrese su contraseña:");
+
+  while (!esAlfanumerica(contraseña)) {
+    alert("La contraseña debe ser alfanumérica.");
+    contraseña = prompt("Ingrese su contraseña nuevamente:");
+  }
+}
+
+function esAlfanumerica(contraseña) {
+  var regex = /^[a-zA-Z0-9]+$/;
+  return regex.test(contraseña);
+}
+
+function mostrarDatos() {
+  console.log("Nombre: " + nombre);
+  console.log("Apellido: " + apellido);
+  console.log("Correo electrónico: " + email);
+  console.log("Contraseña: " + contraseña);
+}
+
+solicitarDatos();
+
+mostrarDatos();
